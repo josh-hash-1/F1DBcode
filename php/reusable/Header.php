d40b6fddc6f1de3907e77d4c451076d2f5db19a6
JDMTL5150 <jacobd2887@gmail.com>
2025-11-13 13:31:44 -0500

    changing path to the image directory

--- php/reusable/Header.php
+++ php/reusable/Header.php
@@ -3,7 +3,7 @@
 
 <nav class="navbar">
     <ul class="menu">
-      <img src="../images/f1db-logo.png" height="50" width="50">
+      <img src="../../images/f1db-logo.png" height="50" width="50">
       <li><a href="../../index.php">Home</a></li>
 
       <!-- Constructors Menu -->
