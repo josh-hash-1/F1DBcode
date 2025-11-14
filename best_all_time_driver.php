da027eb3977d1ab59b19489b803c5657220e8f6e
PencersGitHub <spencer.scarlett@icloud.com>
2025-11-14 15:46:06 -0500

    Added more to style, page working now talk for design plan but ideas in mind

--- best_all_time_driver.php
+++ best_all_time_driver.php
@@ -2,20 +2,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/best_driver.css">
     <title>
         Best Drivers
     </title>
 </head>
 <body>
+    <?php include "Header.php" ?>
     <section id="BestDrivers">
-        <div class="projects-container">
-            <div class="project-card-basic">
+        <div class="project-container">
+            <div class="project-card-top">
                  <h1>TEST</h1>
             </div>
-            <div class="project-card-basic">
+            <div class="project-card-top">
                  
             </div>
-            <div class="project-card-basic">
+            <div class="project-card-top">
                  
             </div>
 
