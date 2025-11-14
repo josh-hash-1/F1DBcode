863ace47ce9b74d2a6c9420e0389fa34afbe04dd
Josh Hash <hash0191@algonquinlive.com>
2025-11-13 19:50:31 -0500

    Formated active teams for mobile and somewhat fixed it for desktop

--- constructors_current.php
+++ constructors_current.php
@@ -13,7 +13,10 @@
     <div class="car-box">
         <img src="images/2025mclarencarright.png" height="320" width="auto">
     </div>
-    
+    <p>
+        McLaren is a British Formula 1 team founded in 1963 by New Zealander Bruce McLaren and based at the McLaren Technology Centre in Woking, making it one of the sport’s oldest and most successful outfits. 
+Across decades the team has amassed over 200 Grand Prix wins and multiple world titles, with standout eras in the 1970s, the dominant Prost–Senna partnership of the 1980s, and later championship runs with Mika Häkkinen and Lewis Hamilton using Mercedes power. 
+    </p>
     
     <?php include "Footer.php"; ?>
 </body>
