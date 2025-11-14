92d1519f6d1fcfe8623ab3272dfa8d0de2a13c30
Josh Hash <hash0191@algonquinlive.com>
2025-11-13 20:44:45 -0500

    added teams in order of high to low score in 2024 constructor's title

--- constructors_current.php
+++ constructors_current.php
@@ -1,23 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Current Constructors</title>
     <link rel="stylesheet" href="css/current_constructor.css">
 </head>
+
 <body>
     <?php include "Header.php" ?>
     <h1>Current Teams</h1>
-    <h2>McLaren</h2>
     <div class="car-box">
-        <img src="images/2025mclarencarright.png" height="320" width="auto">
+        <div class="mclaren">
+            <h2>McLaren Mastercard</h2>
+            <div class="car-box-mclaren">
+                <img src="images/2025mclarencarright.png" height="320" width="auto">
+            </div>
+            <p>
+                McLaren is a British Formula 1 team founded in 1963 by New Zealander Bruce McLaren and based at the McLaren Technology
+                Centre in Woking, making it one of the sport’s oldest and most successful outfits.
+                Across decades the team has amassed over 200 Grand Prix wins and multiple world titles, with standout eras in the 1970s,
+                the dominant Prost–Senna partnership of the 1980s, and later championship runs with Mika Häkkinen and Lewis Hamilton using
+                Mercedes power.
+            </p>
+            <button>
+        </div>
+
+        <div class="ferrari">
+            <h2>Ferrari</h2>
+            <div class="car-box-ferrari">
+                <img src="images/2025ferraricarright.png" height="320" width="auto">
+            </div>
+            <p>
+                Scuderia Ferrari is Formula 1’s oldest and most iconic team, celebrated for its rich history, passionate 
+                fanbase, and legendary drivers. Founded in 1929 and competing in F1 since 1950, Ferrari is known for its 
+                distinctive red cars, strong racing heritage, and relentless pursuit of championship success. With a 
+                legacy built on innovation, speed, and tradition, Ferrari remains one of the sport’s most prestigious and 
+                recognizable teams.
+            </p>
+        </div>
+
+        <div class="redbull">
+            <h2>Oracle RedBull Racing</h2>
+            <div class="car-box-redbull">
+                <img src="images/2025redbullracingcarright.png" height="320" width="auto">
+            </div>
+            <p>
+                Red Bull Racing is a championship-winning Formula 1 team known for its aggressive design philosophy, 
+                bold strategy, and strong driver development program. Founded in 2005, the team quickly became a dominant force, 
+                earning multiple Constructors’ and Drivers’ Championships through innovative aerodynamics and high-performance 
+                engineering. Renowned for its partnership with top drivers and cutting-edge technology, Red Bull is consistently one 
+                of the most competitive and influential teams on the grid.
+            </p>
+        </div>
+
+        <div class="mercedes">
+            <h2>Mercedes AMG Petronas</h2>
+            <div class="car-box-mercedes">
+                <img src="images/2025mercedescarright.png" height="320" width="auto">
+            </div>
+            <p>
+                Mercedes-AMG Petronas is one of the most successful modern Formula 1 teams, known for its dominant hybrid-era 
+                performance, technical excellence, and highly refined engineering. Returning as a works team in 2010, Mercedes went on 
+                to secure multiple consecutive Constructors’ and Drivers’ Championships through unmatched efficiency, power unit 
+                innovation, and consistent race execution. The team remains a benchmark for professionalism, precision, and competitive 
+                ßexcellence in Formula 1.
+            </p>
+        </div>
+
+        <div class="aston-martin">
+            <h2>Aston Martin Aramco</h2>
+            <div class="car-box-aston-martin">
+                <img src="images/2025astonmartincarright.png" height="320" width="auto">
+            </div>
+            <p>
+                
+            </p>
+        </div>
     </div>
-    <p>
-        McLaren is a British Formula 1 team founded in 1963 by New Zealander Bruce McLaren and based at the McLaren Technology Centre in Woking, making it one of the sport’s oldest and most successful outfits. 
-Across decades the team has amassed over 200 Grand Prix wins and multiple world titles, with standout eras in the 1970s, the dominant Prost–Senna partnership of the 1980s, and later championship runs with Mika Häkkinen and Lewis Hamilton using Mercedes power. 
-    </p>
-    
+
     <?php include "Footer.php"; ?>
 </body>
+
 </html>
