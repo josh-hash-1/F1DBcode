823d0106a589228f0c2d27e6de4e6026ba517363
Josh Hash <hash0191@algonquinlive.com>
2025-11-21 11:25:36 -0500

    started work on the all time best constructor's

---
+++ best_all_time_team.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/best_teams.css">
+    <title>All-Time Best Teams</title>
+</head>
+<body>
+    <?php include "Header.php"; ?>
+    <div class="full-page">
+        <h1>All-Time Best Constructor's</h1>
+            <div class="Ferarri">
+                <h2>Ferrari</h2>
+                <img src="images/ferrari-logo.png" height="240" widht="auto">
+                <p>
+                    Ferrari stands as the longest-running team in Formula 1 history, holding more 
+                    race entries than anyone else with over 1,100 Grands Prix to their name. With 
+                    nearly 250 victories and 16 Constructors’ Championships, they remain one of 
+                    the sport’s most iconic and successful teams.
+                </p>
+            </div>
+    </div>
+</body>
+</html>
