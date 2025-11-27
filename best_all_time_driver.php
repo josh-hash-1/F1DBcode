d3ca32f724a231578efa0c7ce380a6f496089043
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2025-11-27 16:15:19 -0500

    Btn added, last images, no mult type screen yet

--- best_all_time_driver.php
+++ best_all_time_driver.php
@@ -17,58 +17,81 @@
                     <div class="project-card-image-box">
                         <img src="images/alain_sad.png" alt="">
                     </div>
-                    <p><strong>Josh</strong></p>                    
-                    <p><strong>Josh</strong></p>
+                    <p><strong>4× World Champion</strong></p>
+                    <p><strong>1985, 1986, 1989, 1993</strong></p>
+                    <p><strong>McLaren & Williams</strong></p>
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>                    
                 </div>
 
                 <div class="project-card-top">
-                    <h1>Alain Prost</h1>
+                    <h1>Juan Manuel Fangio</h1>
                     <div class="project-card-image-box">
                         <img src="images/juan_test.png"  alt="">
                     </div>
                     <div class="project-card-body"></div>
-                    <p><strong>Josh</strong></p>
-                    <p><strong>Josh</strong></p>
+                    <p><strong>5× World Champion</strong></p>
+                    <p><strong>1951, 1954, 1955, 1956, 1957</strong></p>
+                    <p><strong>Alfa Romeo, Maserati, Mercedes & Ferrari</strong></p>
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
                 </div>
 
                 <div class="project-card-top">
-                    <h1>Ham Lew</h1>
+                    <h1>Lewis Hamilton</h1>
                     <div class="project-card-image-box">
                         <img src="images/Lewis-on-car.jpg" alt="">
                     </div>
-                    <p><strong>Josh</strong></p>                    
-                    <p><strong>Josh</strong></p>
+                    <p><strong>7× World Champion</strong></p>
+                    <p><strong>2008, 2014, 2015, 2017, 2018, 2019, 2020</strong></p>
+                    <p><strong>McLaren & Mercedes</strong></p>
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
                 </div>
 
                 <div class="project-card-top">
-                    <h1>Alain Prost</h1>
+                    <h1>Max Verstappen</h1>
                     <div class="project-card-image-box">
                         <img src="images/max_test.png"  alt="">
                     </div>
                     <div class="project-card-body"></div>
-                    <p><strong>Josh</strong></p>
-                    <p><strong>Josh</strong></p>
+                    <p><strong>3× World Champion</strong></p>
+                    <p><strong>2021, 2022, 2023</strong></p>
+                    <p><strong>Red Bull Racing</strong></p>
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
                 </div>
 
                 <div class="project-card-top">
-                    <h1>Ham Lew</h1>
+                    <h1>Michael Schumacher</h1>
                     <div class="project-card-image-box">
-                        <img src="images/Lewis-on-car.jpg" alt="">
+                        <img src="images/Michael_Schumacher.jpg" alt="">
                     </div>
-                    <p><strong>Josh</strong></p>                    
-                    <p><strong>Josh</strong></p>
+                    <p><strong>7× World Champion</strong></p>
+                    <p><strong>1994, 1995, 2000, 2001, 2002, 2003, 2004</strong></p>
+                    <p><strong>Benetton & Ferrari</strong></p>
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
                 </div>
 
                 <div class="project-card-top">
-                    <h1>Alain Prost</h1>
+                    <h1>Sebastian Vettel</h1>
                     <div class="project-card-image-box">
-                        <img src="images/max_test.png"  alt="">
+                        <img src="images/sebby_test.jpg"  alt="">
                     </div>
                     <div class="project-card-body"></div>
-                    <p><strong>Josh</strong></p>
-                    <p><strong>Josh</strong></p>
+                    <p><strong>4× World Champion</strong></p>
+                    <p><strong>2010, 2011, 2012, 2013</strong></p>
+                    <p><strong>Red Bull Racing</strong></p>
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
                 </div>
-                
             </div>
 
         </div>
