bdb0e86b69f1e9dd530c084be3d99afcaa22df24
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2025-11-28 16:48:05 -0500

    Done Hall of Fame, added new drivers, fixes to screen, links, formatting

--- best_all_time_driver.php
+++ best_all_time_driver.php
@@ -11,91 +11,136 @@
 </head>
 <body>
     <?php include "Header.php" ?>
+        <div class="title_first">
+            <h1>Top Drivers - Hall of Fame</h1>
+        </div>
         <div class="project-container">
-                <div class="project-card-top">
-                    <h1>Alain Prost</h1>
-                    <div class="project-card-image-box">
-                        <img src="images/alain_sad.png" alt="">
-                    </div>
-                    <p><strong>4× World Champion</strong></p>
-                    <p><strong>1985, 1986, 1989, 1993</strong></p>
-                    <p><strong>McLaren & Williams</strong></p>
-                    <button class="project-card-top-btn">
-                        Click me!
-                    </button>                    
+            <div class="project-card-top">
+                <h1>Juan Manuel Fangio</h1>
+                <div class="project-card-image-box">
+                    <img src="images/juan_test.png"  alt="">
                 </div>
-
-                <div class="project-card-top">
-                    <h1>Juan Manuel Fangio</h1>
-                    <div class="project-card-image-box">
-                        <img src="images/juan_test.png"  alt="">
-                    </div>
-                    <div class="project-card-body"></div>
-                    <p><strong>5× World Champion</strong></p>
-                    <p><strong>1951, 1954, 1955, 1956, 1957</strong></p>
-                    <p><strong>Alfa Romeo, Maserati, Mercedes & Ferrari</strong></p>
+                <div class="project-card-body"></div>
+                <p><strong>5× World Champion</strong></p>
+                <p><strong>1951, 1954, 1955, 1956, 1957</strong></p>
+                <p><strong>Alfa Romeo, Maserati, Mercedes & Ferrari</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Juan_Manuel_Fangio" target="_blank">
                     <button class="project-card-top-btn">
                         Click me!
                     </button>   
+                </a>  
+            </div>
+            <div class="project-card-top">
+                <h1>Niki Lauda</h1>
+                <div class="project-card-image-box">
+                    <img src="images/niki_lauda.png"  alt="Photo of Niki Lauda">
                 </div>
-
-                <div class="project-card-top">
-                    <h1>Lewis Hamilton</h1>
-                    <div class="project-card-image-box">
-                        <img src="images/Lewis-on-car.jpg" alt="">
-                    </div>
-                    <p><strong>7× World Champion</strong></p>
-                    <p><strong>2008, 2014, 2015, 2017, 2018, 2019, 2020</strong></p>
-                    <p><strong>McLaren & Mercedes</strong></p>
+                <div class="project-card-body"></div>
+                <p><strong>3× World Champion</strong></p>
+                <p><strong>1975, 1977, 1984</strong></p>
+                <p><strong>Ferrari & Mclaren</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Niki_Lauda" target="_blank">
                     <button class="project-card-top-btn">
                         Click me!
                     </button>   
-                </div>
+                </a>    
+            </div>
 
-                <div class="project-card-top">
-                    <h1>Max Verstappen</h1>
-                    <div class="project-card-image-box">
-                        <img src="images/max_test.png"  alt="">
-                    </div>
-                    <div class="project-card-body"></div>
-                    <p><strong>3× World Champion</strong></p>
-                    <p><strong>2021, 2022, 2023</strong></p>
-                    <p><strong>Red Bull Racing</strong></p>
+            <div class="project-card-top">
+                <h1>Alain Prost</h1>
+                <div class="project-card-image-box">
+                    <img src="images/alain_sad.png" alt="">
+                </div>
+                <p><strong>4× World Champion</strong></p>
+                <p><strong>1985, 1986, 1989, 1993</strong></p>
+                <p><strong>McLaren & Williams</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Alain_Prost" target="_blank">
                     <button class="project-card-top-btn">
                         Click me!
                     </button>   
-                </div>
+                </a>                    
+            </div>
 
-                <div class="project-card-top">
-                    <h1>Michael Schumacher</h1>
-                    <div class="project-card-image-box">
-                        <img src="images/Michael_Schumacher.jpg" alt="">
-                    </div>
-                    <p><strong>7× World Champion</strong></p>
-                    <p><strong>1994, 1995, 2000, 2001, 2002, 2003, 2004</strong></p>
-                    <p><strong>Benetton & Ferrari</strong></p>
+            <div class="project-card-top">
+                <h1>Ayrton Senna</h1>
+                <div class="project-card-image-box">
+                    <img src="images/ayrton_senna.png"  alt="">
+                </div>
+                <div class="project-card-body"></div>
+                <p><strong>3× World Champion</strong></p>
+                <p><strong>1988, 1990, 1991</strong></p>
+                <p><strong>Mclaren</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Ayrton_Senna" target="_blank">
                     <button class="project-card-top-btn">
                         Click me!
                     </button>   
-                </div>
+                </a>  
+            </div>
 
-                <div class="project-card-top">
-                    <h1>Sebastian Vettel</h1>
-                    <div class="project-card-image-box">
-                        <img src="images/sebby_test.jpg"  alt="">
-                    </div>
-                    <div class="project-card-body"></div>
-                    <p><strong>4× World Champion</strong></p>
-                    <p><strong>2010, 2011, 2012, 2013</strong></p>
-                    <p><strong>Red Bull Racing</strong></p>
+            <div class="project-card-top">
+                <h1>Michael Schumacher</h1>
+                <div class="project-card-image-box">
+                    <img src="images/Michael_Schumacher.jpg" alt="">
+                </div>
+                <p><strong>7× World Champion</strong></p>
+                <p><strong>1994, 1995, 2000, 2001, 2002, 2003, 2004</strong></p>
+                <p><strong>Benetton & Ferrari</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Michael_Schumacher" target="_blank">
                     <button class="project-card-top-btn">
                         Click me!
                     </button>   
+                </a>    
+            </div>
+
+            <div class="project-card-top">
+                <h1>Lewis Hamilton</h1>
+                <div class="project-card-image-box">
+                    <img src="images/Lewis-on-car.jpg" alt="">
                 </div>
+                <p><strong>7× World Champion</strong></p>
+                <p><strong>2008, 2014, 2015, 2017, 2018, 2019, 2020</strong></p>
+                <p><strong>McLaren & Mercedes</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Lewis_Hamilton" target="_blank">
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
+                </a>   
+            </div>
+
+            <div class="project-card-top">
+                <h1>Sebastian Vettel</h1>
+                <div class="project-card-image-box">
+                    <img src="images/sebby_test.jpg"  alt="">
+                </div>
+                <div class="project-card-body"></div>
+                <p><strong>4× World Champion</strong></p>
+                <p><strong>2010, 2011, 2012, 2013</strong></p>
+                <p><strong>Red Bull Racing</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Sebastian_Vettel" target="_blank">
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
+                </a>    
+            </div>
+
+            <div class="project-card-top">
+                <h1>Max Verstappen</h1>
+                <div class="project-card-image-box">
+                    <img src="images/max_test.png"  alt="">
+                </div>
+                <div class="project-card-body"></div>
+                <p><strong>3× World Champion</strong></p>
+                <p><strong>2021, 2022, 2023</strong></p>
+                <p><strong>Red Bull Racing</strong></p>
+                <a href="https://en.wikipedia.org/wiki/Max_Verstappen" target="_blank">
+                    <button class="project-card-top-btn">
+                        Click me!
+                    </button>   
+                </a>   
             </div>
 
         </div>
-            <?php include "Footer.php" ?>
-
+    </div>
+    <?php include "Footer.php" ?>
 </body>
 </html>
