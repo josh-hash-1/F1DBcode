0b3bdb86ccb7b622c8952013955e38a761615483
PencersGitHub <spencer.scarlett@icloud.com>
2025-11-29 15:06:26 -0500

    Placeholder text + CSS expended to about us

--- about.php
+++ about.php
@@ -11,8 +11,31 @@
     <?php include "Header.php" ?>
     <div class="about-us-container">
         <div class="card-main">
-            <h1>Test test</h1>
-            <p>Hello World</p>
+            <div class="title">
+                <h1>About Us</h1>
+            </div>
+
+             <div class="body-text">
+                <h2>Disclaimer</h2>
+                <p>
+                    This page isn't so much for an about us section but more for a delivery checklist for what we completed
+                    throughtout the project.
+                </p>
+            </div>
+
+            <div class="body-text">
+                <h2>Front End Components</h2>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae omnis dicta ab maxime et alias doloribus aliquid, veritatis consequatur dolores pariatur, voluptates repudiandae, deserunt cum reiciendis? Rem voluptates fuga repudiandae.
+                </p>
+            </div>
+
+            <div class="body-text">
+                <h2>Back End Components</h2>
+                <p> 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus harum a sunt. Doloribus, error expedita quia tempore ratione blanditiis at culpa delectus, quas alias voluptatum corporis vero perferendis sequi!
+                </p>
+            </div>
         </div>
     </div>
 
