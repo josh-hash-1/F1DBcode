06e7407a4408c95a3dcc91bb8f3c1d742e5b818e
PencersGitHub <spencer.scarlett@icloud.com>
2025-11-29 20:08:59 -0500

    Added on to about us text parts

--- about.php
+++ about.php
@@ -26,14 +26,32 @@
             <div class="body-text">
                 <h2>Front End Components</h2>
                 <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae omnis dicta ab maxime et alias doloribus aliquid, veritatis consequatur dolores pariatur, voluptates repudiandae, deserunt cum reiciendis? Rem voluptates fuga repudiandae.
+                    Every page was created with clean, standards-enforced HTML, using semantic elements and meaningful tags, resulting in a well structured, responsive layout. 
+                    The visual theme is maintained across pages through the careful placement of content blocks, imagery, and card-style components, creating a polished, professional look. 
+                    It combines Bootstrap's global layout system with custom CSS to create a responsive, user-friendly experience. 
+                    The flow on every page is smooth across devices and screen sizes, allowing seamless transitions and consistent usability across desktops, tablets, and mobile devices. 
+                    Interactive elements include user input forms, buttons, and navigation features, with purposeful animations to reinforce overall usability. 
+                    Tied with subtile animations to support the design without distracting from the base content of the page, creating an design that feels modern and engaging.
                 </p>
+                <ul>
+                    <li>All HTML files are valid and semantic</li>
+                    <li>CSS is organized and responsive</li>
+                    <li>Bootstrap is properly implemented</li>
+                </ul>
             </div>
 
             <div class="body-text">
                 <h2>Back End Components</h2>
                 <p> 
+                    Will includes things about backend, CRUD ops., database usage, php, etc... 
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi accusamus harum a sunt. Doloribus, error expedita quia tempore ratione blanditiis at culpa delectus, quas alias voluptatum corporis vero perferendis sequi!
+                </p>
+            </div>
+
+            <div class="body-text">
+                <h2>Misc. Components</h2>
+                <p> 
+                    Will include things about readme, comments, member names, etc...
                 </p>
             </div>
         </div>
